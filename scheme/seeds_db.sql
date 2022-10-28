53cc41a073c4adeb1f5998ecb2733e0d871ebfd0
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-10-28 20:26:30 +0500

    db scheme fixed and seeds script created

--- scheme/seeds_db.sql
+++ scheme/seeds_db.sql
@@ -1,43 +1,4 @@
-﻿CREATE DATABASE catering;
-
-CREATE SCHEMA public;
-
-CREATE TABLE client (
-    id BIGSERIAL NOT NULL
-            CONSTRAINT client_id_pk
-                PRIMARY KEY,
-    name VARCHAR (25) NOT NULL
-);
-
-CREATE TABLE catering_point (
-    id BIGSERIAL NOT NULL
-            CONSTRAINT catering_point_id_pk
-                PRIMARY KEY,
-    name VARCHAR (25) NOT NULL
-);
-
-CREATE TABLE client_catering_point (
-    id BIGSERIAL NOT NULL
-            CONSTRAINT client_catering_point_id_pk
-                PRIMARY KEY,
-    client_id INTEGER NOT NULL,
-    catering_point_id INTEGER NOT NULL,
-    day_of_week INTEGER NOT NULL
-        CHECK (day_of_week BETWEEN 1 AND 7),
-    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-    CONSTRAINT fk_client
-        FOREIGN KEY (client_id)
-            REFERENCES client(id),
-    CONSTRAINT fk_catering_point
-        FOREIGN KEY (catering_point_id)
-            REFERENCES catering_point(id)
-);
-
-CREATE UNIQUE INDEX client_id_idx on client (id);
-CREATE UNIQUE INDEX catering_point_id_idx on catering_point (id);
-CREATE UNIQUE INDEX client_catering_point_id_idx on client_catering_point (id);
-
-INSERT INTO catering_point (id, name) VALUES (1, 'Ultimate Food');
+﻿INSERT INTO catering_point (id, name) VALUES (1, 'Ultimate Food');
 INSERT INTO catering_point (id, name) VALUES (2, 'Cater Me Mexican');
 INSERT INTO catering_point (id, name) VALUES (3, 'The Better Caterer');
 INSERT INTO catering_point (id, name) VALUES (4, 'Catering Masters');

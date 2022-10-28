d8ced2cd7952dfcfe357bb77efaa18cc2a8d514a
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-10-28 21:41:19 +0500

    added stored procedure and history table

--- scheme/catering_db.sql
+++ scheme/catering_db.sql
@@ -33,32 +33,22 @@ CREATE TABLE client_catering_point (
             REFERENCES catering_point(id)
 );
 
+CREATE TABLE client_catering_change_history (
+    id BIGSERIAL NOT NULL
+        CONSTRAINT client_catering_change_history_id_pk
+            PRIMARY KEY,
+    created_at DATE NOT NULL DEFAULT now(),
+    client_id INTEGER NOT NULL,
+    catering_point_id INTEGER NOT NULL,
+    event INTEGER NOT NULL,
+    CONSTRAINT fk_client
+        FOREIGN KEY (client_id)
+            REFERENCES client(id),
+    CONSTRAINT fk_catering_point
+        FOREIGN KEY (catering_point_id)
+            REFERENCES catering_point(id)
+);
+
 CREATE UNIQUE INDEX client_id_idx on client (id);
 CREATE UNIQUE INDEX catering_point_id_idx on catering_point (id);
 CREATE UNIQUE INDEX client_catering_point_id_idx on client_catering_point (id);
-
-INSERT INTO catering_point (id, name) VALUES (1, 'Ultimate Food');
-INSERT INTO catering_point (id, name) VALUES (2, 'Cater Me Mexican');
-INSERT INTO catering_point (id, name) VALUES (3, 'The Better Caterer');
-INSERT INTO catering_point (id, name) VALUES (4, 'Catering Masters');
-INSERT INTO catering_point (id, name) VALUES (5, 'Cater Nation');
-
-INSERT INTO client (id, name) VALUES (1, 'VK');
-INSERT INTO client (id, name) VALUES (2, 'Yandex');
-INSERT INTO client (id, name) VALUES (3, 'EPAM');
-INSERT INTO client (id, name) VALUES (4, 'Google');
-INSERT INTO client (id, name) VALUES (5, 'Ubisoft');
-INSERT INTO client (id, name) VALUES (6, 'DevSoft');
-
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (1, 2, 1);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (5, 4, 5);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (5, 3, 4);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (5, 1, 2);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 1);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 2);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 3);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 4);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 5);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (3, 5, 6);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week) VALUES (6, 1, 6);
-INSERT INTO client_catering_point (client_id, catering_point_id, day_of_week, is_deleted) VALUES (6, 5, 3, TRUE);
---
+++ scheme/sp_db.sql
@@ -0,0 +1,65 @@
+﻿CREATE OR REPLACE FUNCTION catering_change_by_client_history_fn (
+        client_id_in BIGINT
+    )
+    RETURNS TABLE (
+        created_at DATE,
+        name VARCHAR (25),
+        event TEXT
+        ) AS
+$Body$
+    SELECT ccch.created_at,
+           c.name,
+           ccch.event
+    FROM client_catering_change_history ccch
+    JOIN client c on ccch.client_id = c.id
+    WHERE c.id = client_id_in;
+$Body$
+LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION client_change_by_catering_history_fn (
+        catering_id_in BIGINT
+    )
+    RETURNS TABLE (
+        created_at DATE,
+        name VARCHAR (25),
+        event TEXT
+        ) AS
+$Body$
+    SELECT ccch.created_at,
+           cp.name,
+           ccch.event
+    FROM client_catering_change_history ccch
+    JOIN catering_point cp on ccch.catering_point_id = cp.id
+    WHERE cp.id = catering_id_in;
+$Body$
+LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION client_change_by_catering_history_all_fn ()
+    RETURNS TABLE (
+        created_at DATE,
+        name VARCHAR (25),
+        event TEXT
+        ) AS
+$Body$
+    SELECT ccch.created_at,
+           cp.name,
+           ccch.event
+    FROM client_catering_change_history ccch
+    JOIN catering_point cp on ccch.catering_point_id = cp.id;
+$Body$
+LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION catering_change_by_client_history_all_fn ()
+    RETURNS TABLE (
+        created_at DATE,
+        name VARCHAR (25),
+        event TEXT
+        ) AS
+$Body$
+    SELECT ccch.created_at,
+           c.name,
+           ccch.event
+    FROM client_catering_change_history ccch
+    JOIN client c on ccch.client_id = c.id;
+$Body$
+LANGUAGE sql;

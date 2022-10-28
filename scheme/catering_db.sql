598f5fc4927ea5a813d8ed735eaa15d2aac1b592
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-10-28 19:57:51 +0500

    added db tables script

---
+++ scheme/catering_db.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE client (
+    id BIGSERIAL NOT NULL
+            CONSTRAINT client_id_pk
+                PRIMARY KEY,
+    name VARCHAR (25) NOT NULL 
+);
+
+
+CREATE TABLE catering_point (
+    id BIGSERIAL NOT NULL
+            CONSTRAINT catering_point_id_pk
+                PRIMARY KEY,
+    name VARCHAR (25) NOT NULL 
+);
+
+
+CREATE TABLE client_catering_point (
+    id BIGSERIAL NOT NULL
+            CONSTRAINT catering_point_id_pk
+                PRIMARY KEY,
+    client_id INTEGER NOT NULL,
+    catering_point_id INTEGER NOT NULL,
+    day_of_week INTEGER NOT NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT fk_client
+        FOREIGN KEY (client_id) 
+            REFERENCES client(id),
+    CONSTRAINT fk_catering_point
+        FOREIGN KEY (catering_point_id) 
+            REFERENCES catering_point(id)
+);
+
+CREATE UNIQUE INDEX client_id_idx on client (id);
+CREATE UNIQUE INDEX catering_point_id_idx on catering_point (id);
+CREATE UNIQUE INDEX client_catering_point_id_idx on client_catering_point (id);
